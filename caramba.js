68c086b2d869a416fdf519e46e536b712722fb1a
Augusto Gonçalves Dias <augustodias2008@gmail.com>
2024-08-04 19:37:41 -0300

    versao final

---
+++ caramba.js
@@ -0,0 +1,8 @@
+let res = document.getElementById('res')
+
+function aperta(){
+    res.innerHTML = "1 kbps = 1000 bits"
+    res.innerHTML = "1 mbps = 1000000"
+    res.innerHTML = "1 gbps = 1e+9"
+    res.innerHTML = "1 tbps = 1e+12"
+}
